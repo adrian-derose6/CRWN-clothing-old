e17bdc1a060f18c1588dc5d770dbbeeac98d8554
Adrian DeRose <adrian.derose6@gmail.com>
2019-10-28 18:06:23 -0500

    Style menu items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+
+import './homepage.styles.scss';
 
 import HomePage from './homepage.component';
 
--- src/homepage.component.js
+++ src/homepage.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './homepage.styles.scss';
 
 const HomePage = () => {
     return (
@@ -10,32 +11,24 @@ const HomePage = () => {
                         <span className='subtitle'>SHOP NOW</span>
                     </div>
                 </div>
-            </div>
-            <div className="directory-menu">
                 <div className='menu-item'>
                     <div className='content'>
                         <h1 className='title'>HATS</h1>
                         <span className='subtitle'>SHOP NOW</span>
                     </div>
                 </div>
-            </div>
-            <div className="directory-menu">
                 <div className='menu-item'>
                     <div className='content'>
                         <h1 className='title'>JACKETS</h1>
                         <span className='subtitle'>SHOP NOW</span>
                     </div>
                 </div>
-            </div>
-            <div className="directory-menu">
                 <div className='menu-item'>
                     <div className='content'>
                         <h1 className='title'>WOMENS</h1>
                         <span className='subtitle'>SHOP NOW</span>
                     </div>
                 </div>
-            </div>
-            <div className="directory-menu">
                 <div className='menu-item'>
                     <div className='content'>
                         <h1 className='title'>MENS</h1>
