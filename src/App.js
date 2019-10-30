4449029dbf23bb1bd768b6d22a5a47e0153e5f15
Adrian DeRose <adrian.derose6@gmail.com>
2019-10-29 21:13:05 -0500

    Render basic list of collection items in Shop page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import HatsPage from './pages/hatspage/hatspage.component';
+import ShopPage from './pages/shop/shop.component.js';
 import './App.css';
 
 
@@ -10,7 +11,7 @@ function App() {
   return (
     <div className="App">
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
