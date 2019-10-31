afe727ac4c833562e97d518cde2c8bed7b4c7472
Adrian DeRose <adrian.derose6@gmail.com>
2019-10-31 05:27:59 -0500

    Save user sign in data to current user value in App state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shop/shop.component.js';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component.js';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import './App.scss';
 
 
@@ -23,8 +23,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth= auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth= auth.onAuthStateChanged(async userAuth => {
+      
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+        })
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
