0bb7b66b503233583e5d99a16383bbc3f2c34787
Adrian DeRose <adrian.derose6@gmail.com>
2019-11-05 19:55:47 -0600

    Remove unnecessary imports from App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutPage from './pages/checkout/checkout.component.js';
 import HomePage from './pages/homepage/homepage.component';
-import HatsPage from './pages/hatspage/hatspage.component';
 import ShopPage from './pages/shop/shop.component.js';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component.js';
 import TopBanner from './components/top-banner/top-banner.component';
 import Footer from './components/footer/footer.component.js';
 
-import { setCurrentUser, checkUserSession } from './redux/user/user.actions.js';
+import { checkUserSession } from './redux/user/user.actions.js';
 
-import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
 import { selectCurrentUser } from './redux/user/user.selectors.js';
 import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
