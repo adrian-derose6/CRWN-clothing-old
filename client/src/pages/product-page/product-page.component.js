872c0ad8287d48ce41cc650ed7fb9161249be9aa
Adrian DeRose <adrian.derose6@gmail.com>
2020-04-29 19:25:00 -0500

    Set selectedArticle in state when ProductPage receives productDetails prop

--- client/src/pages/product-page/product-page.component.js
+++ client/src/pages/product-page/product-page.component.js
@@ -19,23 +19,49 @@ const BackgroundImage = styled.img`
 `
 
 class ProductPage extends React.Component {
+    state = {
+        selectedArticle: null
+    };
+
     componentDidMount() {
         const { match, fetchProductDetails, productDetails } = this.props;
         const { articleCode } = match.params; 
 
         fetchProductDetails({ articleCode });
+    }
 
+    componentDidUpdate(prevProps) {
+        const { productDetails } = this.props;
+        const { selectedArticle } = this.state;
+
+        if (productDetails && !selectedArticle) {    
+            const { articlesList } = productDetails;
+            articlesList.forEach(item => {
+                if (item.color.code === productDetails.color.code) {
+                    this._setSelectedArticle(item);
+                    return;
+                }
+            });
+        }
+    }
+
+    _setSelectedArticle = (article) => {
+        this.setState({ selectedArticle: article });
     }
 
     render() {
         const { productDetails } = this.props;
+        const { selectedArticle } = this.state;
 
-        if (!productDetails) return <Spinner />;
-
-        const { articlesList, description } = productDetails;
-        console.log(productDetails)
-        const galleryDetails = articlesList[0].galleryDetails;
+        if (!productDetails || !selectedArticle) return <Spinner />;
+        
+        console.log(selectedArticle);
+        const { description, fits, code } = productDetails;
+        const galleryDetails = selectedArticle.galleryDetails;
         const productDescription = description || '';
+        const fit = fits ? fits[0] : null;
+        const compositions = selectedArticle.compositions;
+        const modelHeight = selectedArticle.modelHeight;
 
         return (
             <div className='product-page'>
@@ -53,7 +79,38 @@ class ProductPage extends React.Component {
                             })}
                         </div>
                         <div className='info-box'>
-                            <p className='description-text'>{description}</p>
+                            <div className='info-content'>
+                                <p className='description-text'>{description}</p>
+                                <dl className='description-list'>
+                                    { (modelHeight) ? (
+                                        <div className='description-list-item'>
+                                            <dt>Size</dt>
+                                            <dd>{`This model is ${modelHeight}.`}</dd>                                       
+                                        </div>
+                                        )
+                                        : null
+                                    }
+                                    { (fit) ?
+                                        <div className='description-list-item'>
+                                            <dt>Fit</dt>
+                                            <dd>{fit}</dd>                                       
+                                        </div>
+                                        : null
+                                    }
+                                    <div className='description-list-item'>
+                                        <dt>Composition</dt>
+                                        <dd>{compositions.map((comp, index) => {
+                                                const materials = comp.materials.map((mat) => `${mat.name} ${mat.percentage}%`).join(', ');
+                                                return comp.compositionType ? `${comp.compositionType}: ${materials}, ` : `${materials}`
+                                            })}
+                                        </dd>                                       
+                                    </div>
+                                    <div className='description-list-item'>
+                                        <dt>Article No.</dt>
+                                        <dd>{code}</dd>                                       
+                                    </div>
+                                </dl>
+                            </div>
                         </div>
                         <div className='image-row'>
                             { galleryDetails.map((item, index) => {
